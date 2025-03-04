6d9e3f572bcf8cdb58bb8054e3ca4323a897834b
phammings <ryanlepham9@gmail.com>
2025-03-03 23:48:14 -0500

    feat: implemented create expense page

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,9 +1,21 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/create-expense')({
   component: CreateExpense,
 })
 
 function CreateExpense() {
-  return <div className="p-2">Create-expense</div>
+  return <div className="p-2">
+    <h2>Create Expense</h2>
+    <form className="max-w-xl m-auto">
+      <Label htmlFor="title">Title</Label>
+      <Input type="text" id="title" placeholder="Title" />
+      <Label htmlFor="amount">Amuont</Label>
+      <Input type="text" id="amount" placeholder="Amuont" />
+      <Button className="mt-4" type="submit">Create Expense</Button>
+    </form>
+  </div>
 }
