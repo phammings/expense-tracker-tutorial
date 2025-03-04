aabb5850d42ac67f84591e8635d3f1cdc2f62a68
phammings <ryanlepham9@gmail.com>
2025-03-04 11:50:45 -0500

    feat: implemented kinde authentication

--- server/app.ts
+++ server/app.ts
@@ -2,12 +2,13 @@ import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { serveStatic } from 'hono/bun';
 import { expensesRoute } from './routes/expenses'
+import { authRoute } from './routes/auth';
 
 const app = new Hono();
 
 app.use('*', logger());
 
-const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute);
+const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute).route("/", authRoute);
 
 app.get('*', serveStatic( {root: './frontend/dist'} ));
 app.get('*', serveStatic( {path: './frontend/dist/index.html'} ));
